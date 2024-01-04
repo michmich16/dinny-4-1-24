a857a452b46f8a831132bc7cecd98d6b9306fbf8
Michael P <michpouls@gmail.com>
2024-01-04 10:38:10 +0100

    vdfv

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -1,2 +1,26 @@
+
+
 const myGallery = ['Dinny-01.jpg', 'Dinny-02.jpg', 'Dinny-03.jpg', 'Dinny-04.jpg', 'Dinny-05.jpg', 'Dinny-06.jpg', 'Dinny-07.jpg', 'Dinny-08.jpg', 'Dinny-09.jpg'];
 
+let myImageCount = 0;
+let myGalleryElement = document.getElementById("gallery_image")
+
+let myForwardButton = document.getElementById("gallery_forward")
+
+myForwardButton.addEventListener('click'), (e) => {
+
+    myImageCount++;
+
+    if (myImageCount >= myGallery.length) {
+        myImageCount = 0
+    }
+    showImage()
+}
+
+showImage();
+
+// vises billede i dom
+function showImage(){
+    let myURL = 'assets/images/GFX/' + myGallery[myImageCount];
+    myGalleryElement.src = myURL;
+}
