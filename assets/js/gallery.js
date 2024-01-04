e1067b94a04ce941afa6f1511e0fe9eac36c8fc2
Michael P <michpouls@gmail.com>
2024-01-04 10:19:10 +0100

    ferfe

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -1,3 +1,2 @@
-const myGallery = ['Dinny-01.jpg', 'Dinny-02.jpg', 'Dinny-03.jpg', 'Dinny-04.jpg', 'Dinny-05.jpg', 'Dinny-06.jpg', 'Dinny-07.jpg', 'Dinny-08.jpg', 'Dinny-09.jpg']
+const myGallery = ['Dinny-01.jpg', 'Dinny-02.jpg', 'Dinny-03.jpg', 'Dinny-04.jpg', 'Dinny-05.jpg', 'Dinny-06.jpg', 'Dinny-07.jpg', 'Dinny-08.jpg', 'Dinny-09.jpg'];
 
-console.log=(myGallery)
